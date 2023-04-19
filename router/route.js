a9f02ab0356c8f743b4cb4fec12082aea32a2636
unknown <akmalmaulfi@gmail.com>
2023-04-19 22:04:06 +0700

    create authentication middleware

--- router/route.js
+++ router/route.js
@@ -2,8 +2,10 @@ const express = require("express")
 const app = express()
 const users = require("./userRoute")
 const reflect = require("./reflectionRoute")
+const { authentication } = require("../middleware/authentication")
 
 app.use("/users", users)
+app.use(authentication) // -> Auth Middleware
 app.use("/reflections", reflect)
 
 module.exports = app
