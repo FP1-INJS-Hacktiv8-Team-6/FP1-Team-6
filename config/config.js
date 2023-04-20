4a53cda87d3a1685e27d58edff8e03a74edce39c
nnvi <118540005+nnvi@users.noreply.github.com>
2023-04-20 18:44:29 +0700

    resolve config

--- config/config.js
+++ config/config.js
@@ -3,9 +3,14 @@ const { Pool } = require("pg")
 const config = new Pool({
   user: "postgres",
   host: "127.0.0.1",
+<<<<<<< users-feature
   database: "FP1_ReflectionAPI",
   password: "akmal",
   port: 5432,
+=======
+  database: "reflectionAPI",
+  password: "123",
+>>>>>>> main
 })
 
 module.exports = config
