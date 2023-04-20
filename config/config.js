6b36cbfd5dee5f681915ec0d571fe01be58f4e3d
nnvi <118540005+nnvi@users.noreply.github.com>
2023-04-20 18:38:57 +0700

    Remove conflict markers

--- config/config.js
+++ config/config.js
@@ -1,19 +1,10 @@
 const Pool = require('pg')
 
-<<<<<<< Updated upstream
-const config =new Pool({
-    user : "postgres",
-    host:"localhost",
-    database : "FP1_team6",
-    password : "020711",
-    port :5432
-=======
 const config = new Pool({
   user: "postgres",
   host: "127.0.0.1",
   database: "reflectionAPI",
   password: "123",
->>>>>>> Stashed changes
 })
 
 module.exports = config
