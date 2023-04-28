2a94c2c77b693fe93918b3ee6444ab194abf42ba
nnvi <vinnie.felim@gmail.com>
2023-04-28 22:39:10 +0700

    add Swagger Documentation

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT
 const route = require("./router/route")
+
+//Swagger add>>>>>
+const swaggerUi = require('swagger-ui-express');
+const path = require('path');
+const YAML = require('yamljs');
+
+const swaggerDocument = YAML.load(path.join(__dirname, './openapi.yaml'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+//<<<< swagger done
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
